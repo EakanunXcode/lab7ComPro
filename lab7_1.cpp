4166afab141b8fd47ef11af5c470777e06a53fe1
Eakanun <Eakanun48@gmail.com>
2023-12-08 14:34:54 +0700

    lab7 Complete

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
+    int a = 20, b = 30;
+    float c = 4, out;
+    out = a - b/c;
+    cout << "output: "<< out <<"\n";
     return 0;
 }
